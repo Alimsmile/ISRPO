9bc9b8425e580dea2cca0b9f6336c9ebd0de98f1
Alimsmile <alimov.insur@mail.ru>
2022-03-30 19:37:10 +0300

    3

---
+++ et --hard add main.cpp
@@ -0,0 +1,83 @@
+[33mcommit ffbdea4a950a3e4ffe2964f465213c46b2696af8[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/third_branch[m[33m, [m[1;31morigin/master[m[33m, [m[1;32mthird_branch[m[33m)[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 19:26:10 2022 +0300
+
+    1
+
+[33mcommit 981f7d4c3edb47ee640150095fc17741a30186c2[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 19:24:04 2022 +0300
+
+    add files
+
+[33mcommit 01531ae1a56424a72b006b00cd0d270d5c66f71a[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 19:22:00 2022 +0300
+
+    first
+
+[33mcommit f0edf3ab56e1a87a370e2dcb683d641ec4be3dad[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 19:16:28 2022 +0300
+
+    edit
+
+[33mcommit 58837fbd580b226e632fab88b3757379f1be7d9d[m[33m ([m[1;31morigin/second_branch[m[33m, [m[1;32msecond_branch[m[33m)[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 19:03:58 2022 +0300
+
+    add  main.cpp
+
+[33mcommit a23269716c56180d367259155c901a76d14b4607[m[33m ([m[1;31morigin/new_branch[m[33m, [m[1;32mnew_branch[m[33m)[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 19:02:35 2022 +0300
+
+    add head.cpp
+
+[33mcommit a65b1bfddb1ce8c8dbc3f351ccde8d5f518d7177[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 18:55:41 2022 +0300
+
+    edit Text.txt.
+
+[33mcommit 80f275dd43d59f9e2c91ad6d1b79f9efe1ae6dd4[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 18:52:09 2022 +0300
+
+    add Text.txt
+
+[33mcommit 2a42950cd33f367d07249ce0f3a3654b0f231c7c[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 18:47:36 2022 +0300
+
+    delete main.cpp
+
+[33mcommit 7a3d5ecfa7a875386262e830cc5b00174f671b2c[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 18:45:29 2022 +0300
+
+    add hello world
+
+[33mcommit a9e9340fa8e5aa8b9eeee6a7f1e606a4047af68d[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 18:41:32 2022 +0300
+
+    add main foo
+
+[33mcommit d2522b5c5eb5a397e140b02f3e4698db9276ac2a[m
+Author: Alimsmile <90337242+Alimsmile@users.noreply.github.com>
+Date:   Wed Mar 30 10:01:22 2022 +0300
+
+    Create README.md
+
+[33mcommit 862d7a73e700a1ab57c9fb48297086b5d5492b2d[m[33m ([m[1;31morigin/Test[m[33m, [m[1;32mTest[m[33m)[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 09:55:56 2022 +0300
+
+    added gitignore
+
+[33mcommit 113a86a94c8cf43b5b7ef7f3c077b7274039455f[m
+Author: Alimsmile <alimov.insur@mail.ru>
+Date:   Wed Mar 30 09:52:52 2022 +0300
+
+    'comment'
